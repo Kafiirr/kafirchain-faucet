38151150daee15af395b8d6d43fba61cf08ae457
Kafiirr <kafiirr.666@gmail.com>
2024-09-27 10:19:16 +0700

    update faucet

--- components/Faucet.tsx
+++ components/Faucet.tsx
@@ -38,56 +38,50 @@ export default function Faucet() {
     <div
       className="h-screen flex justify-center items-center p-4"
       style={{
-        backgroundImage: "url('https://i.pinimg.com/736x/b0/23/ac/b023ac8b97119e6cbc6968669b630781.jpg')",
+        backgroundImage: "url('https://c4.wallpaperflare.com/wallpaper/103/633/205/abstract-beijing-black-and-white-building-wallpaper-preview.jpg')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="relative w-full max-w-sm flex-col rounded-xl bg-neutral-800 text-gray-50 shadow-md">
-        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-          <div className="w-full space-y-8">
-            <div>
-              <img className="mx-auto h-[70px] w-15" src="logo.png" alt="$KFR Faucet" />
-              <h2 className="mt-2 text-center text-2xl font-bold tracking-tight text-gray-300">$KFR Faucet</h2>
-            </div>
-            <form className="mt-1 space-y-6" onSubmit={handleSubmit}>
-              <div className="-space-y-px rounded-md shadow-sm">
-                <div>
-                  <input
-                    id="address"
-                    name="address"
-                    type="string"
-                    required
-                    className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Input Your EVM Addr Here"
-                  />
-                </div>
-              </div>
-              <div className="flex justify-center">
-                <HCaptcha
-                  sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY as string}
-                  onVerify={(token, ekey) => handleVerificationSuccess(token, ekey)}
-                />
-              </div>
-              <div>
-                <button
-                  disabled={isDisabled}
-                  type="submit"
-                  className="disabled:opacity-25 group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Request $KFR
-                </button>
-                <div className="mt-2 text-center">
-                  <p className="text-xs text-gray-300 mb-1">Have more $KFR? Send back to:</p>
-                  <p className="text-xs border border-gray-400 rounded-md p-1 inline-block">
-                    0xA6097E61738B3f5a851ac051309BeA24235820Ba
-                  </p>
-                </div>
-              </div>
-            </form>
-          </div>
+      <div className="w-full max-w-sm">
+        <div className="text-center">
+          <img className="mx-auto h-[70px] w-15" src="logo.png" alt="$KFR Faucet" />
+          <h2 className="mt-2 text-center text-2xl font-bold tracking-tight text-gray-300">$KFR Faucet</h2>
         </div>
+        <form className="mt-4 space-y-6" onSubmit={handleSubmit}>
+          <div>
+            <input
+              id="address"
+              name="address"
+              type="string"
+              required
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              placeholder="Input Your EVM Addr Here"
+            />
+          </div>
+          <div className="flex justify-center">
+            <HCaptcha
+              sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY as string}
+              onVerify={(token, ekey) => handleVerificationSuccess(token, ekey)}
+            />
+          </div>
+          <div>
+            <button
+              disabled={isDisabled}
+              type="submit"
+              className="disabled:opacity-25 group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Request $KFR
+            </button>
+            <div className="mt-2 text-center">
+              <p className="text-xs text-white mb-1">Have more $KFR? Send back to:</p>
+              <p className="text-xs border border-white rounded-md p-2 bg-black/40 text-white inline-block">
+                0xA6097E61738B3f5a851ac051309BeA24235820Ba
+              </p>
+            </div>
+          </div>
+        </form>
       </div>
       <SuccessModal message={successMessage} />
       <ErrorModal message={errorMessage} />
